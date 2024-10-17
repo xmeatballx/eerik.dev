286f7ab4575cc089d322211e090162c0832b8125
xmeatballx <erik.jensen5@pcc.edu>
2024-10-16 22:47:12 -0400

    fix nix blog post excerpt

--- src/content/blog.rs
+++ src/content/blog.rs
@@ -14,7 +14,7 @@ pub static BLOG_POSTS: &'static[&'static BlogPost] = &[
     &BlogPost{ 
         title: "My Nixos Laptop Setup",
         excerpt: "I like customizing things. So when the hard drive on my laptop died, wiping my Windows install that I had been running for 3 years...",
-        slug: "homelab",
+        slug: "nixos-laptop",
         markdown_path: "nixos.md",
         published_date: "10/17/24",
         featured_image: "/assets/images/nixos.png",
